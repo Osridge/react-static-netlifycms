598f3685f14a34f472eb1507ea9d15836a7e8226
Patrick <patch2908@gmail.com>
2018-04-20 14:37:09 -0500

    Added home html and global scripts/css

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,18 +3,34 @@ import { withSiteData } from 'react-static'
 //
 import logoImg from '../logo.png'
 
-// function changeToBlack(){
-//   console.log('changed')
-//   document.body.style.backgroundColor = "gray"
-// }
-// changeToBlack()
 
 export default withSiteData(() => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to <br/> the jungle.</h1>
-    {/* <img src={logoImg} alt="" /> */}
-    <div class="testUpload">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non sapien commodo, eleifend ante id, viverra mi. Proin tincidunt arcu lorem, at elementum justo luctus id. Pellentesque suscipit accumsan est in blandit. Suspendisse suscipit tempor augue. Phasellus eget dolor nec quam elementum volutpat eget sit amet justo. Quisque gravida, tortor id fermentum pulvinar, odio quam blandit elit, ac vehicula leo urna vitae justo. Etiam quis viverra libero, vel mattis quam. Nullam gravida, est vitae dignissim viverra, nisi libero convallis elit, a laoreet orci eros in magna. Nunc auctor, sem in euismod eleifend, libero dui mattis sapien, a gravida tortor ex tristique quam. Suspendisse interdum, massa in gravida luctus, purus turpis elementum lacus, id fermentum libero enim id orci.</p>
-    </div>
+    <section id="header" class="parallax-window" data-parallax="scroll" data-image-src="/wp-content/uploads/2017/08/hero-lobaugh.jpg" data-position-y="top" data-speed="0.5">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-9 col-xs-push-1 col-sm-8 col-sm-push-2 text-center">
+            <h2>If You're hurt and</h2>
+            <h2 class="large">Need Help</h2>
+            <h2>We Will Fight For Your Rights</h2>
+            <p><a href="/contact" class="btn btn-primary">Free Consultation</a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="about">
+    </section>
+    <section id="experience">
+    </section>
+    <section id="areas">
+    </section>
+    <section id="action">
+    </section>
+    <section id="videos">
+    </section>
+    <section id="contact">
+    </section>
+    <section id="blogs">
+    </section>
   </div>
 ))
