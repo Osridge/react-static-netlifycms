bc4a2ab17f93fa6104335b99dce9672249dabc86
Patrick <patch2908@gmail.com>
2018-04-20 09:53:47 -0500

    Changed line-height

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,7 +9,6 @@ export default withSiteData(() => (
     {/* <img src={logoImg} alt="" /> */}
     <div class="testUpload">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non sapien commodo, eleifend ante id, viverra mi. Proin tincidunt arcu lorem, at elementum justo luctus id. Pellentesque suscipit accumsan est in blandit. Suspendisse suscipit tempor augue. Phasellus eget dolor nec quam elementum volutpat eget sit amet justo. Quisque gravida, tortor id fermentum pulvinar, odio quam blandit elit, ac vehicula leo urna vitae justo. Etiam quis viverra libero, vel mattis quam. Nullam gravida, est vitae dignissim viverra, nisi libero convallis elit, a laoreet orci eros in magna. Nunc auctor, sem in euismod eleifend, libero dui mattis sapien, a gravida tortor ex tristique quam. Suspendisse interdum, massa in gravida luctus, purus turpis elementum lacus, id fermentum libero enim id orci.</p>
-    <p>isjeogtjsejrt</p>
     </div>
   </div>
 ))
